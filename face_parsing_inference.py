323b0a5a50f96c725cbcee856de5f1ad0ba80838
Nguyen Thanh <Thanh.CEAA.K58.HUST@gmail.com>
2021-05-29 17:44:58 +0700

    Temporarily complete draft

--- face_parsing_inference.py
+++ face_parsing_inference.py
@@ -6,18 +6,115 @@ import numpy as np
 import cv2
 
 import torch
+import torch.nn as nn
 from torchvision import transforms
+from torchvision.utils import make_grid
 
 import matplotlib.pyplot as plt
 
 from model import BiSeNet
 
+import net
+from function import adaptive_instance_normalization, coral
+
+
+def test_transform(size, crop):
+    transform_list = []
+    if size != 0:
+        transform_list.append(transforms.Resize(size))
+    if crop:
+        transform_list.append(transforms.CenterCrop(size))
+    transform_list.append(transforms.ToTensor())
+    transform = transforms.Compose(transform_list)
+    return transform
+
+
+def style_transfer(vgg, decoder, content, style, device, alpha=1.0,
+                   interpolation_weights=None):
+    assert (0.0 <= alpha <= 1.0)
+    content_f = vgg(content)
+    style_f = vgg(style)
+    if interpolation_weights:
+        _, C, H, W = content_f.size()
+        feat = torch.FloatTensor(1, C, H, W).zero_().to(device)
+        base_feat = adaptive_instance_normalization(content_f, style_f)
+        for i, w in enumerate(interpolation_weights):
+            feat = feat + w * base_feat[i:i + 1]
+        content_f = content_f[0:1]
+    else:
+        feat = adaptive_instance_normalization(content_f, style_f)
+    feat = feat * alpha + content_f * (1 - alpha)
+    return decoder(feat)
+
+
+def initialize_model(vgg_checkpoint, decoder_checkpoint, device):
+    decoder = net.decoder
+    vgg = net.vgg
+
+    decoder.eval()
+    vgg.eval()
+
+    decoder.load_state_dict(torch.load(decoder_checkpoint))
+    vgg.load_state_dict(torch.load(vgg_checkpoint))
+    vgg = nn.Sequential(*list(vgg.children())[:31])
+
+    vgg.to(device)
+    decoder.to(device)
+
+    return vgg, decoder
+
+
+def run_forward(content, style, content_tf, style_tf, vgg, decoder, device,
+                preserve_color, alpha, interpolation_weights):
+    assert content.shape[2] == 3 and style.shape[2] == 3
+    content = Image.fromarray(content)
+    style = Image.fromarray(style)
+    content = content_tf(content)
+    style = style_tf(style)
+
+    if preserve_color:
+        style = coral(style, content)
+
+    style = style.to(device).unsqueeze(0)
+    content = content.to(device).unsqueeze(0)
+
+    with torch.no_grad():
+        output = style_transfer(vgg, decoder, content, style, device, alpha, interpolation_weights)
+
+    if torch.cuda.is_available():
+        output = output.cpu()
+
+    output = make_grid(output)
+    output = output.mul(255).add_(0.5).clamp_(0, 255).permute(1, 2, 0).to("cpu", torch.uint8).numpy()
+
+    return output
 
 def get_args():
     parser = argparse.ArgumentParser()
 
     parser.add_argument("--save_pth", type=str, default="res/cp/79999_iter.pth")
     parser.add_argument("--image_path", type=str, default=None)
+    parser.add_argument("--overlay_path", type=str, default=None)
+
+    parser.add_argument('--vgg', type=str, default='models/vgg_normalised.pth')
+    parser.add_argument('--decoder', type=str, default='models/decoder.pth')
+
+    parser.add_argument('--content_size', type=int, default=0,
+                        help='New (minimum) size for the content image, \
+                    keeping the original size if set to 0')
+    parser.add_argument('--style_size', type=int, default=0,
+                        help='New (minimum) size for the style image, \
+                    keeping the original size if set to 0')
+    parser.add_argument('--crop', action='store_true',
+                        help='do center crop to create squared image')
+    parser.add_argument('--preserve_color', action='store_true',
+                        help='If specified, preserve color of the content image')
+    parser.add_argument('--alpha', type=float, default=1.0,
+                        help='The weight that controls the degree of \
+                             stylization. Should be between 0 and 1')
+    parser.add_argument(
+        '--style_interpolation_weights', type=str, default=None,
+        help='The weight for blending the style of multiple style images')
 
     args = parser.parse_args()
 
@@ -29,9 +126,31 @@ if __name__ == '__main__':
 
     save_pth = args.save_pth
     image_path = args.image_path
+    overlay_path = args.overlay_path
+
+    vgg = args.vgg
+    decoder = args.decoder
+
+    content_size = args.content_size
+    style_size = args.style_size
+    crop = args.crop
+    preserve_color = args.preserve_color
+    alpha = args.alpha
+
+    if args.style_interpolation_weights is not None:
+        weights = [int(i) for i in args.style_interpolation_weights.split(',')]
+        interpolation_weights = [w / sum(weights) for w in weights]
+    else:
+        interpolation_weights = None
+
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+    vgg, decoder = initialize_model(vgg_checkpoint=vgg, decoder_checkpoint=decoder, device=device)
+
+    content_tf = test_transform(content_size, crop)
+    style_tf = test_transform(style_size, crop)
 
     n_classes = 19
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
     part_colors = [[255, 0, 0], [255, 85, 0], [255, 170, 0],
                    [255, 0, 85], [255, 0, 170],
@@ -70,18 +189,81 @@ if __name__ == '__main__':
 
     image = np.array(image)
 
-    vis_mask = np.array([list(map(lambda x: part_colors[x], row)) for row in parsing]).astype(np.uint8)
+    overlay = Image.open(overlay_path)
+    overlay = overlay.resize((image.shape[1], image.shape[0]))
+    overlay = np.array(overlay)
 
-    vis_img = cv2.addWeighted(image[:, :, ::-1], 0.4, vis_mask, 0.6, 0)
+    #parsing = cv2.resize(parsing, (overlay.shape[1], overlay.shape[0]), interpolation=cv2.INTER_NEAREST)
+    #image = cv2.resize(image, (overlay.shape[1], overlay.shape[0]))
 
-    cv2.imshow("Anh", vis_img)
+    #mask = np.where(parsing, 255 * np.ones_like(parsing), np.zeros_like(parsing)).astype(np.uint8)
+    #transform_mask = cv2.erode(mask, kernel=np.ones((51, 51), dtype=np.uint8), iterations=1)
+    #cv2.imshow("Mask", mask)
+    #cv2.imshow("Transform mask", transform_mask)
+    #cv2.waitKey(0)
+    hair_mask = np.where(np.isin(parsing, [16, 17, 18]), True, False)
+    skin_mask = np.where(np.isin(parsing, list(range(1, 16))), True, False)
+    mask = np.where(hair_mask[:, :, np.newaxis], image, np.zeros_like(image))
+    cv2.imshow("Mask", mask)
     cv2.waitKey(0)
 
-    for i in range(n_classes):
-        vis_mask = np.array([list(map(lambda x: part_colors[x] if x == i else [0, 0, 0], row)) for row in parsing]).astype(np.uint8)
+    stylized_hair = run_forward(mask, overlay, content_tf, style_tf, vgg, decoder, device,
+                                preserve_color, alpha, interpolation_weights)
 
-        vis_img = cv2.addWeighted(image[:, :, ::-1], 0.4, vis_mask, 0.6, 0)
+    stylized_hair = cv2.resize(stylized_hair, (mask.shape[1], mask.shape[0]))
 
-        cv2.imshow(str(i), vis_img)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
+    #hair_mask = cv2.dilate(hair_mask.astype(np.uint8), np.ones((21, 21), dtype=np.uint8))
+    #stylized_hair = np.where(hair_mask[:, :, np.newaxis], stylized_hair, image)
+    #stylized_hair = np.where(hair_mask[:, :, np.newaxis], stylized_hair, overlay)
+    stylized_hair = np.where(parsing[:, :, np.newaxis], stylized_hair, overlay)
+
+    cv2.imshow("Output", stylized_hair[:, :, ::-1])
+    cv2.waitKey(0)
+
+    dilated_skin_mask = (cv2.dilate(skin_mask.astype(np.uint8), np.ones((21, 21), dtype=np.uint8)) * 255).astype(np.uint8)
+    dilated_skin_mask = np.where(dilated_skin_mask, True, False)
+    eroded_skin_mask = (cv2.erode(skin_mask.astype(np.uint8), np.ones((21, 21), dtype=np.uint8)) * 255).astype(np.uint8)
+    eroded_skin_mask = np.where(eroded_skin_mask, True, False)
+    res_skin_mask = np.logical_xor(eroded_skin_mask, dilated_skin_mask)
+    #res_skin_mask = res_skin_mask.astype(np.uint8) * 255
+    cv2.imshow("Skin mask", res_skin_mask.astype(np.uint8) * 255)
+    cv2.waitKey(0)
+
+    #image = np.where(res_skin_mask[:, :, np.newaxis], cv2.GaussianBlur(image, ksize=(21, 21), sigmaX=0), image)
+    kernel_size = max(max(image.shape[0], image.shape[1]) // 10 , 31)
+    if kernel_size % 2 == 0:
+        kernel_size += 1
+    print("kernel_size: {}".format(kernel_size))
+    smooted_dilated_skin_mask = cv2.GaussianBlur(skin_mask.astype(np.float32), (kernel_size, kernel_size), sigmaX=20., sigmaY=20.)
+    #smooted_dilated_skin_mask = cv2.filter2D(dilated_skin_mask.astype(np.uint8), -1, np.ones((75, 75), dtype=np.float32) / (75 * 75))
+
+    #_, cnts, _ = cv2.findContours((dilated_skin_mask.astype(np.uint8) * 255).copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    #print(np.unique(dilated_skin_mask.astype(np.uint8) * 255))
+    ## based on contour area, get the maximum contour which is the hand
+    #segmented = max(cnts, key=cv2.contourArea)
+    #print("segmented: {}".format(segmented))
+
+    #for cnt in cnts:
+    #    x, y, w, h = cv2.boundingRect(cnt)
+    #    print(x, y, w, h)
+
+    #print("cnts: {}".format(len(cnts)))
+
+    #x, y, w, h = cv2.boundingRect(cnts[0])
+    #cX = x + w // 2
+    #cY = y + h // 2
+    #print(cX, cY)
+    #center = (int(image.shape[1] / 2), int(image.shape[0] / 2))
+    #center = (cX, cY)
+
+    #result = cv2.seamlessClone(image, stylized_hair, skin_mask[:, :, np.newaxis], center, cv2.MIXED_CLONE)
+
+    #parsing = np.where(np.isin(parsing, [0, 16]), False, True)
+    #result = np.where(parsing[:, :, np.newaxis] > 0, cv2.addWeighted(image[:, :, ::-1], 0.8, overlay[:, :, ::-1], 0.2, 0), overlay[:, :, ::-1])
+    #result = np.where(parsing[:, :, np.newaxis] > 0, stylized_hair[:, :, ::-1], overlay[:, :, ::-1])
+
+    #result = np.where(dilated_skin_mask[:, :, np.newaxis], image, stylized_hair)
+    result = smooted_dilated_skin_mask[:, :, np.newaxis] * image + (1 - smooted_dilated_skin_mask[:, :, np.newaxis]) * stylized_hair
+    cv2.imwrite("result.jpg", result[:, :, ::-1])
+    cv2.imshow("Anh", result[:, :, ::-1])
+    cv2.waitKey(0)
